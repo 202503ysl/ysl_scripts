0ff66cd52dd521725852df0a534a80a7571fe243
202503ysl <163137145+202503ysl@users.noreply.github.com>
2024-04-04 11:21:36 +0800

    Add files via upload

--- yhzc/yhzc.py
+++ yhzc/yhzc.py
@@ -2,27 +2,27 @@ import asyncio
 
 from ysl_yhzc import *
 
+
 """
 实物
 cron: 0 0 10 * * *
-new Env("一嗨租车")
-抓取微信小程序 一嗨租车 的 备注, userid, token  以#拼接
+new Env("一橙租车")
+抓取微信小程序 一橙租车 的 备注, userid, token  以#拼接
 eg:
     https://appmini.1hai.cn/Wechat/User/Base/CacheInfo?%2F%2FW7FhThpifw%2FS22t55d3Y%24z22AyX22xiOCV5j8t0g*
     userid == %2F%2FW7FhThpifw%2FS22t55d3Y%24z22AyX22xiOCV5j8t0g*
     token == 请求头的token
 
     备注#userid#token
+    
+环境必须为 python 3.10
+两个文件必须在同一目录
+使用青龙上传 必须点击退出保存 不支持arm
 多账号以 "回车" 或者 多变量 隔开
-青龙变量为 yczc_ck
+青龙变量为 yhzc_ck
 """
-
 print("""
 正在运行脚本：【一嗨租车】V1.0
-
-警告: 本脚本限制环境为 python版本为3.10 并且不支持arm环境. 
-网络问题请自行解决 需能访问 raw.githubusercontent.com
-
 📢 请认真阅读以下声明
  【免责声明】   
 📌 脚本文件仅用于测试和学习研究   
@@ -33,6 +33,7 @@ print("""
 📌 脚本文件如有不慎被破解或修改由破解或修改者承担   
 📌 如不接受此条款请立即删除脚本文件
 """)
+
 
 if __name__ == '__main__':
     # 脚本配置区
